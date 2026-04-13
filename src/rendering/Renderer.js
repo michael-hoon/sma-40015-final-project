7e38f5ed7ea8bff33d0a7cda73cfa754029e39b0
michael-hoon <michaelhoon@gmail.com>
2026-04-13 22:18:15 +0800

    fix: white background pixi canvas

--- src/rendering/Renderer.js
+++ src/rendering/Renderer.js
@@ -69,7 +69,7 @@ export default class Renderer {
     await this._app.init({
       width:           CONFIG.GRID_WIDTH  * CELL_SIZE,
       height:          CONFIG.GRID_HEIGHT * CELL_SIZE,
-      backgroundColor: 0xfafafa,
+      backgroundColor: 0x091220,
       antialias:       true,
       resolution:      window.devicePixelRatio || 1,
       autoDensity:     true,
